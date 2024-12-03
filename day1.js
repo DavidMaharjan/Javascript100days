6bd6efa63bcabf22d35963e0b804979ed2d163f1
David Maharjan <dmaharjan224@gmail.com>
2024-12-03 08:19:13 +0545

    day2 numbers and math

--- day1.js
+++ day1.js
@@ -1,10 +1,15 @@
-alert('Hello');
-undefined
-alert('Good job');
-undefined
-2 + 2
-4
-10 - 3
-7
-document.body.innerHTML='hello';
-'hello'
+//lesson 1 basics
+
+// alert('Hello');
+// undefined
+// alert('Good job');
+// undefined
+// 2 + 2
+// 4
+// 10 - 3
+// 7
+// document.body.innerHTML='hello';
+// 'hello'
+
+
+
