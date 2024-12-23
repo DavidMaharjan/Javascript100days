6b8507cd53bde57e0b29aa41e7a6b3aa2be8f4df
David Maharjan <dmaharjan224@gmail.com>
2024-12-23 18:59:49 +0545

    blackjack card diplaying changes

--- blackjack_game/challenges.js
+++ blackjack_game/challenges.js
@@ -16,3 +16,96 @@
 // else {
 //     console.log("Not eligible, you have already gotten one")
 // }
+
+//arrays - ordered list of elements
+let featuredPosts=[
+    "Check out my Netflix clone",
+    "link to github",
+    "I've just relaunced my portfolio"
+]
+
+// let experience=[
+//     "I've worked on some projects",
+//     "I have a compputer engineering degree",
+//     "I have MERN certification"
+// ]
+
+// console.log(experience[0]+". "+featuredPosts[2])
+
+//arrays are composite data types differnt dataypes can be put in an array
+
+// let description =[
+// "David Maharjan",
+// 23,
+// true
+// ]
+// console.log(description)
+// console.log(typeof(description[2]))
+
+//adding and removing items from array
+
+//  let cards=[7,4]
+//  cards.push(6)
+//  console.log(cards)
+
+// let messages=[
+//     "Hey, how's it going?",
+//     "I'm great, thank you! How about you?",
+//     "All good. Been working on my portfolio lately"
+// ]
+
+// let newMessage="Same here!"
+
+// messages.push(newMessage)
+// console.log(messages)
+// messages.pop()
+// console.log(messages)
+
+
+//for loops in javascript
+
+// for (let count=10; count<=20;count+=1){
+//     console.log(count)
+// }
+
+// for (let count=10; count<=100 ;count+=10){
+//     console.log(count)
+// }
+
+//for loops and arrays
+
+// let messages=[
+//     "Hey, how's it going?",
+//     "I'm great, thank you! How about you?",
+//     "All good. Been working on my portfolio lately",
+//     "Same here"
+// ]
+
+// for (i=0;i<messages.length;i++){
+//     console.log(messages[i])
+// }
+
+// let cards=[7,3,9]
+// for (i=0;i<=cards.length;i++){
+//     console.log(cards[i])
+// }
+
+let player1Time =102
+let player2Time=107
+
+function getFastestTime(){
+    if (player1Time<player2Time){
+        return player1Time
+    }
+    else {
+        return player2Time
+    }
+}
+// let fastestRaceTime = getFastestTime()
+// console.log(fastestRaceTime)
+
+function gettotalRaceTime(){
+    return player1Time+player2Time
+}
+let totalRaceTime=gettotalRaceTime()
+console.log(totalRaceTime)
--- blackjack_game/index.js
+++ blackjack_game/index.js
@@ -1,5 +1,5 @@
-let firstCard =11
-let secondCard=10
+let firstCard = getRandomCards()
+let secondCard=getRandomCards()
 let sum = firstCard + secondCard
 let hasBlackJack=false
 let isAlive=true
@@ -10,11 +10,25 @@ let message_el=document.getElementById('message-el')
 //let sum_el=document.getElementById('sum-el')
 let sumEl=document.querySelector('#sum-el')
 let cardEl=document.querySelector('#card-el')
+let cards=[firstCard,secondCard]//array -ordered list of items
 
-
+function getRandomCards(){
+    return 5
+}
 function startGame(){
+    renderGame()
+}
+function renderGame(){
 
-    cardEl.textContent="Cards: " + firstCard + "  " + secondCard
+    //renderout firstcard and secondcard
+    cardEl.textContent="Cards: "
+    for (let i=0;i<cards.length;i++)
+    {
+    cardEl.textContent+= cards[i] + " "
+    // console.log(cards[i])
+
+
+    }
     sumEl.textContent="Sum: "+sum
     if (sum<=20){
         message="Do you want to draw a new card?"
@@ -30,4 +44,17 @@ function startGame(){
     message_el.textContent=message
     
 }
+function newCard(){
 
+    let card = getRandomCards()
+    sum+=card
+    cards.push(card)
+    // console.log(cards)
+    renderGame()
+    
+}
+// let sentence=["Hello", "my","name","is","David"]
+// let greetingEl=document.getElementById("greetingEl")
+// for (i=0;i<sentence.length;i++){
+//     greetingEl.textContent+=" "+sentence[i]
+// }
