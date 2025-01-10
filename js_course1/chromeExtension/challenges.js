3440f82a3dbbfc5e3b5fc87dfe9aa9d2c05e5c4a
David Maharjan <dmaharjan224@gmail.com>
2025-01-10 13:13:58 +0545

    floder restructred

---
+++ js_course1/chromeExtension/challenges.js
@@ -0,0 +1,4 @@
+const playerName ="David"
+let credits = 45
+credits =credits - 10
+
---
+++ js_course1/chromeExtension/index.js
@@ -0,0 +1,13 @@
+
+let myLeads=[]
+const inputEl= document.getElementsById("input-el")
+const inputBtn =document.getElementById("input-btn")
+inputBtn.addEventListener("click",function(){
+    console.log("Save button clicked")
+
+})
+
+// let boxBtn=document.getElementById("box")
+// boxBtn.addEventListener("click", function(){
+//     console.log("I want to open box")
+// })
